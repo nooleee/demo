5e41b96dd7a37ad72da741303190bcb611b14069
nooleee <wjssnfl7739@naver.com>
2024-05-14 11:47:31 +0900

    Modify BookList Component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,7 @@
+import { Box, Heading, Input, Table, TableContainer, Tbody, Tfoot, Th, Thead, Tr, Td, Icon, HStack, Button, Stack, IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import React, { useEffect, useRef, useState } from 'react';
+import { MdOutlineOndemandVideo } from "react-icons/md";
+import { WiDaySunny  , WiLightning  } from "react-icons/wi";
 
 const BookList = () => {
     // useState는 화면 랜더링에 반영됨
@@ -9,23 +12,28 @@ const BookList = () => {
     // useRef 는 화면 랜더링에 반영되지 않는 참조값
     const pageCount = useRef(1);
 
-    const fetchBooks = async() => {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const color = useColorModeValue('Purple 500', 'Purple 200');
+    const buttonScheme = useColorModeValue('blackAlpha', 'whiteAlpha');
+
+
+    const fetchBooks = async () => {
         const response = await fetch(
-            `https://dapi.kakao.com/v2/search/vclip?query=${search}&page=${page}`, 
-            {   
-                method : "GET",
-                headers : {
-                    Authorization: `KakaoAK${process.env.REACT_APP_API_KEY}`,
+            `https://dapi.kakao.com/v2/search/vclip?query=${search}&page=${page}`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `KakaoAK ${process.env.REACT_APP_API_KEY}`,
                 },
             }
         );
         const data = await response.json();
         // console.log(data);
 
-        pageCount.current = 
-        data.meta.pageable_count % 10 > 0 
-            ? data.meta.pageable_count / 10 + 1 
-            : data.meta.pageable_count / 10;
+        pageCount.current =
+            data.meta.pageable_count % 10 > 0
+                ? data.meta.pageable_count / 10 + 1
+                : data.meta.pageable_count / 10;
         pageCount.current = Math.floor(pageCount.current);
         pageCount.current = pageCount.current > 15 ? 15 : pageCount.current;
         console.log(pageCount.current);
@@ -36,7 +44,7 @@ const BookList = () => {
 
     const changeSearch = e => {
         // 내용 작성
-        if(e.target.value.length >=2) {
+        if (e.target.value.length >= 2) {
             setSearch(e.target.value);
         }
     }
@@ -48,22 +56,53 @@ const BookList = () => {
 
     return (
         <>
-            <h1>동영상 검색 목록</h1>
-            <input type="text" placeholder='검색어 입력' onChange={changeSearch} />
-            <div>
-                {bookList.map((book) => (
-                    <>
-                        <p>{book.title}</p>
-                    </>
-                ))}
-            </div>
-            <ul>
-                {Array.from({length: pageCount.current}, (_, index) => (
-                    <>
-                        <li onClick={e => {setPage(index + 1)} }>{index + 1}</li>
-                    </>
-                ))}
-            </ul>
+            <Box>
+                <Heading color={color}>
+                    <Icon as={MdOutlineOndemandVideo} boxSize="1.5em" />동영상 검색 목록
+                </Heading>
+
+                {
+                    colorMode === "light" ? 
+                    <IconButton icon={<WiLightning />} onClick={toggleColorMode} size={"lg"}></IconButton> :
+                    <IconButton icon={<WiDaySunny />} onClick={toggleColorMode} size={"lg"}></IconButton>
+                }
+
+                <Input type="text" placeholder='검색어 입력' onChange={changeSearch} size="lg" variant={"filled"} />
+                <TableContainer>
+                    <Table variant={"striped"} colorScheme="purple">
+                        <Thead>
+                            <Tr>
+                                <Th>No</Th>
+                                <Th>Title</Th>
+                                <Th>Authoe</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {bookList.map((book, index) => (
+                                <>
+                                    <Tr>
+                                        <Td>{(page - 1) * 10 + index + 1}</Td>
+                                        <Td><a href={book.url}>{book.title}</a></Td>
+                                        <Td>{book.author}</Td>
+                                    </Tr>
+                                </>
+                            ))}
+                        </Tbody>
+                        <Tfoot></Tfoot>
+                    </Table>
+                </TableContainer>
+                <HStack>
+                    {Array.from({ length: pageCount.current }, (_, index) => (
+                        <>
+                            <Button colorScheme={
+                                page === index + 1 ?
+                                "purple" : buttonScheme
+                            } 
+                            onClick={(e) => { setPage(index + 1) }}>{index + 1}</Button>
+                        </>
+                    ))}
+                    </HStack>
+            </Box>
         </>
     );
 };
